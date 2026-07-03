7d846307a116761928ae5d51639673e0c8a4a7e3
naphat67103382 <naphat.pa@mail.ac.wu.th>
2026-07-03 10:17:24 +0700

    Update homepage

--- fr.js
+++
@@ -1,9 +0,0 @@
-function myfun() {
-  const body = document.getElementById();
-
-  if (body.classList.contains("dark-mode")) {
-    body.classList.remove("dark-mode"); // กลับเป็นโหมดกลางวัน
-  } else {
-    body.classList.add("dark-mode"); // เปลี่ยนเป็นโหมดกลางคืน
-  }
-}
